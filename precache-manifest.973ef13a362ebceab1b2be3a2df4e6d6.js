d8e7833caeb64d80b588446d37a024e3731ba20d
Max Dempsey <maxjdempsey@gmail.com>
2018-11-03 03:22:26 -0400

    Updates

---
+++ precache-manifest.973ef13a362ebceab1b2be3a2df4e6d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee75fb704a1f1879b576",
+    "url": "/Dempsey89.io/ReactShoppingCart/react-shopping-cart/static/js/runtime~main.ee75fb70.js"
+  },
+  {
+    "revision": "0cb48455968ec6b8daa0",
+    "url": "/Dempsey89.io/ReactShoppingCart/react-shopping-cart/static/js/main.0cb48455.chunk.js"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "/Dempsey89.io/ReactShoppingCart/react-shopping-cart/static/js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "0cb48455968ec6b8daa0",
+    "url": "/Dempsey89.io/ReactShoppingCart/react-shopping-cart/static/css/main.eb36b443.chunk.css"
+  },
+  {
+    "revision": "c5430870866ab1e03376195d0666b4ed",
+    "url": "/Dempsey89.io/ReactShoppingCart/react-shopping-cart/index.html"
+  }
+];
